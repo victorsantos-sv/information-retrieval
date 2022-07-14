ccc0b6d81f879c0ec723d15daff621c402964f93
SantosVictor1 <victorsantos.cloud@gmail.com>
2022-07-14 16:21:24 -0300

    feature: added nltk for stopwords removing

--- processor/pre_processor.py
+++ processor/pre_processor.py
@@ -1,38 +1,56 @@
 import string
+import re
 from unidecode import unidecode
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
 
 def remove_duplicated_words(data):
     return list(dict.fromkeys(data))
 
 
-def remove_punctuation(data):
+def _remove_punctuation(data):
     for punctuation in string.punctuation:
         data = data.replace(punctuation, '')
 
     return data
 
 
-def sort(data):
+def _sort(data):
     data.sort()
 
     return data
 
 
-def unidecode_data(data):
+def remove_numbers(data):
+    _number_pattern = r'[0-9]'
+
+    return re.sub(_number_pattern, '', data)
+
+
+def _unidecode_data(data):
     _unidecode_terms = list()
 
     for term in data:
         unidecode_str = unidecode(term)
         _unidecode_terms.append(unidecode_str)
 
-    return sort(_unidecode_terms)
+    return _sort(_unidecode_terms)
+
+
+def _remove_stopwords(data):
+    _stopwords = set(stopwords.words('portuguese'))
+
+    tokens = word_tokenize(data)
+
+    return [word for word in tokens if not word.lower() in _stopwords]
 
 
 def process_data(data):
     _data_lowercase = data.lower()
-    _data_punctuation = remove_punctuation(_data_lowercase)
-    _data_split = _data_punctuation.split()
-    _unidecode_terms = unidecode_data(_data_split)
+    _data_without_number = remove_numbers(_data_lowercase)
+    _data_punctuation = _remove_punctuation(_data_without_number)
+    _data_without_stopwords = _remove_stopwords(_data_punctuation)
+    _unidecode_terms = _unidecode_data(_data_without_stopwords)
 
     return _unidecode_terms
